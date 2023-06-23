7462d49de0b1c2c3352955773bc4f181f654949f
Jones <doc.stetsko@gmail.com>
2023-06-23 08:23:47 +0200

    Upload first version.

---
+++ main.py
@@ -0,0 +1,70 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+turtle = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(turtle.go_up, "Up")
+screen.onkey(turtle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(turtle) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect when player reached the wall
+    if turtle.is_at_finish_line():
+        turtle.go_to_star()
+        car_manager.speed_up()
+        scoreboard.level_up()
+
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
